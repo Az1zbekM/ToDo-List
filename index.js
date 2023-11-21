332d66b0fcf71ee0b7d4bc2b8cdb607559f71aef
Az1zbekM <azizbekmamatqulov8001@gmal.com>
2023-11-21 23:12:54 +0500

    AZZA

--- index.js
+++ index.js
@@ -1,63 +1,80 @@
-function enterButton(event){
-      if (event.key === 'Enter') {
-         addToDo();
-      }
+function enterButton(event) {
+   if (event.key === 'Enter') {
+      addToDo();
+   }
 };
 
 
 const toDoList = [
-   {name:'', dueDate:'2023-11-20'},
-   {name:'', dueDate:'2023-11-20'}
+   // { name: '', dueDate: '2023-11-20' },
+   // { name: '', dueDate: '2023-11-20' }
 ];
 
 renderTodoList();
+function updateToDoStatus(index) {
+   const todoObject = toDoList[index];
+   const { name } = todoObject;
+   const { dueDate } = todoObject;
 
-function renderTodoList(){
-
-let toDoListHTML = '';
-
-for (let i = 0; i < toDoList.length; i++) {
-   const todoObject = toDoList[i];
-   // const name = todoObject.name;
-   const {name} = todoObject;
-   // const dueDate = todoObject.dueDate;
-   const {dueDate} = todoObject;
-
-   const html = 
-   `
+   // Update the todo object's name by adding a CSS class
+   const updatedName1 = `<span class="strikethrough">${name}</span>`;
+   const updatedName2 = `<span class="strikethrough">${dueDate}</span>`
+   toDoList[index].name = updatedName1;
+   toDoList[index].dueDate = updatedName2;
+   renderTodoList();
+}
+function renderTodoList() {
+   let toDoListHTML = '';
+// toDoList.shift()
+   for (let i = 0; i < toDoList.length; i++) {
+      const todoObject = toDoList[i];
+      // const name = todoObject.name;
+      const { name } = todoObject;
+      // const dueDate = todoObject.dueDate;
+      const { dueDate } = todoObject;
+      
+      const html =
+         `
    <div class="datascss">${name}</div> 
    <div class="datascss">${dueDate}</div>
    <button onclick="
    toDoList.splice(${i}, 1);
    renderTodoList();
    " class="delete-btn">Delete</button>
+  <button onclick="
+  updateToDoStatus(${i});
+  ">Done</button>
    `;
-   toDoListHTML += html;
-}
+      toDoListHTML += html;
+   }
 
-document.querySelector('.js-todo-list-div')
-.innerHTML = toDoListHTML
+   document.querySelector('.js-todo-list-div')
+      .innerHTML = toDoListHTML
 }
-function addToDo(){
+function addToDo() {
+
    const inputElem = document.querySelector('.js-name-input');
-
    const name = inputElem.value;
-
+   emptySpace(inputElem.value);
    const dateInputElem = document.querySelector('.js-input-dueDate');
    const dueDate = dateInputElem.value
    toDoList.push({
       //name: name, 
-     // dueDate: dueDate,
-     name,
-     dueDate
+      // dueDate: dueDate,
+      name,
+      dueDate
    });
-   
+
    inputElem.value = '';
    renderTodoList();
+
+
+
+
 };
 
-// function emptySpace(){
-//    if (inputElem.value = '') {
-//       alert('please, write a task');
-//    }
-// }
+function emptySpace(value) {
+   if (value === '') {
+      return alert('please, write a task');
+   }
+}
